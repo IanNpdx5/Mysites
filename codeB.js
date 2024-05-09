b7f5b78e4550e4065a961d497cf38e891a4d7bb5
IanNpdx5 <163360899+IanNpdx5@users.noreply.github.com>
2024-05-09 12:54:26 -0700

    Update codeB.js

--- codeB.js
+++ codeB.js
@@ -1,8 +1,10 @@
    var amount = 1;
    var count = 0;
-   var up1p = 10;
-   var up2p = 50;
-   var up3p = 100;
+   var tc = 0;
+   var bp = 0;
+   var up1p = 15;
+   var up2p = 100;
+   var up3p = 500;
    var up4p = 1000;
    var $b = $("#clickerButton");
    var $up1 = $("#up1");
@@ -10,11 +12,22 @@
    var $up3 = $("#up3");
    var $up4 = $("#up4");
    var $count = $("#count");
+   var $APC = $("#amount");
+   var $TC = $("#clicks");
+   var $BP = $("#presses");
    $count.text("Clicks: " + count);
+   $APC.text("Amount per click: " + amount);
+   $TC.text("Total clicks: " + tc);
+   $BP.text("Total presses: " + bp);
     
    var onClick = function() {
        count += amount;
+       tc += amount;
+       bp++;
        $count.text("Clicks: " + count);
+       $APC.text("Amount per click: " + amount);
+       $TC.text("Total clicks: " + tc);
+       $BP.text("Total presses: " + bp);
    };
 
    var sBOnClick = function(price, amounttoadd) {
@@ -22,6 +35,7 @@
            count -= price;
            amount += amounttoadd;
            $count.text("Clicks: " + count);
+           $APC.text("Amount per click: " + amount);
        }
    };
 
@@ -29,8 +43,8 @@
 
    $up1.on("click", function() {sBOnClick(up1p, 1);});
 
-   $up2.on("click", function() {sBOnClick(up2p, 2);});
+   $up2.on("click", function() {sBOnClick(up2p, 10);});
 
-   $up3.on("click", function() {sBOnClick(up3p, 5);});
+   $up3.on("click", function() {sBOnClick(up3p, 20);});
 
-   $up4.on("click", function() {sBOnClick(up4p, 10);});
+   $up4.on("click", function() {sBOnClick(up4p, 100);});
